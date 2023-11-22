ac802718eb9d24ce0a8f7b27681056ce702ffe44
Guilherme Daniel <gui.dani13@gmail.com>
2023-11-21 23:31:38 -0300

    fix

--- src/services/setNewHorta.ts
+++ src/services/setNewHorta.ts
@@ -2,6 +2,6 @@ import { addDoc, collection } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
 export async function setNewHorta(horta: HortaType) {
-  const docRef = await addDoc(collection(db, "Crops"), horta);
+  const docRef = await addDoc(collection(db, "horta"), horta);
   return docRef.id;
 }
